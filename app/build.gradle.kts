c504c54726b9ca57c4674f979561fdb015cfcdb1
Jonathan Steele <iNoles@users.noreply.github.com>
2024-02-20 20:33:00 -0500

    Compose with NDK example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -22,7 +25,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -40,16 +43,32 @@ android {
         }
     }
     buildFeatures {
-        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
--- app/src/main/java/com/jonathansteele/calculatorndk/MainActivity.kt
+++ app/src/main/java/com/jonathansteele/calculatorndk/MainActivity.kt
@@ -1,36 +1,60 @@
 package com.jonathansteele.calculatorndk
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jonathansteele.calculatorndk.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jonathansteele.calculatorndk.ui.theme.CalculatorNDKTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val number1 = binding.number1.text.toString().toInt()
-        val number2 = binding.number2.text.toString().toInt()
-
-        binding.btnAdd.setOnClickListener {
-            add(number1, number2)
-        }
-
-        binding.btnSub.setOnClickListener {
-            subtract(number1, number2)
-        }
-
-        binding.btnMul.setOnClickListener {
-            multiply(number1, number2)
-        }
-
-        binding.btnDiv.setOnClickListener {
-            divide(number1, number2)
+        enableEdgeToEdge()
+        setContent {
+            CalculatorNDKTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val number1State = remember { mutableStateOf(TextFieldValue()) }
+                    val number2State = remember { mutableStateOf(TextFieldValue()) }
+                    val number1 = number1State.value.text.toInt()
+                    val number2 = number2State.value.text.toInt()
+                    CalculatorDisplay(
+                        number1 = number1State,
+                        number2 = number2State,
+                        modifier = Modifier.padding(innerPadding),
+                        addButton = {
+                            add(number1, number2)
+                        },
+                        divideButton = {
+                            divide(number1, number2)
+                        },
+                        subButton = {
+                            subtract(number1, number2)
+                        },
+                        multiButton = {
+                            multiply(number1, number2)
+                        },
+                    )
+                }
+            }
         }
     }
 
@@ -38,15 +62,95 @@ class MainActivity : AppCompatActivity() {
      * A native method that is implemented by the 'calculatorndk' native library,
      * which is packaged with this application.
      */
-    private external fun add(x: Int, y: Int): Int
-    private external fun subtract(x: Int, y: Int): Int
-    private external fun multiply(x: Int, y: Int): Int
-    private external fun divide(x: Int, y: Int): Int
+    private external fun add(
+        x: Int,
+        y: Int,
+    ): Int
+
+    private external fun subtract(
+        x: Int,
+        y: Int,
+    ): Int
+
+    private external fun multiply(
+        x: Int,
+        y: Int,
+    ): Int
+
+    private external fun divide(
+        x: Int,
+        y: Int,
+    ): Int
 
     companion object {
         // Used to load the 'calculatorndk' library on application startup.
         init {
             System.loadLibrary("calculatorndk")
         }
+    }
+}
+
+@Composable
+fun CalculatorDisplay(
+    number1: MutableState<TextFieldValue>,
+    number2: MutableState<TextFieldValue>,
+    modifier: Modifier = Modifier,
+    addButton: () -> Unit,
+    divideButton: () -> Unit,
+    subButton: () -> Unit,
+    multiButton: () -> Unit,
+) {
+    Column(
+        modifier = modifier.padding(top = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        Text(
+            text = "Simple Calculator",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        TextField(
+            value = number1.value,
+            onValueChange = { number1.value = it },
+            placeholder = { Text(text = "Enter number") },
+        )
+        TextField(
+            value = number2.value,
+            onValueChange = { number2.value = it },
+            placeholder = { Text(text = "Enter number") },
+        )
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+        ) {
+            Button(onClick = { addButton() }) {
+                Text(text = "+", fontSize = 30.sp)
+            }
+            Button(onClick = { subButton() }) {
+                Text(text = "-", fontSize = 30.sp)
+            }
+            Button(onClick = { multiButton() }) {
+                Text(text = "x", fontSize = 30.sp)
+            }
+            Button(onClick = { divideButton() }) {
+                Text(text = "÷", fontSize = 30.sp)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalculatorDisplayPreview() {
+    CalculatorNDKTheme {
+        val number1 = remember { mutableStateOf(TextFieldValue()) }
+        val number2 = remember { mutableStateOf(TextFieldValue()) }
+        CalculatorDisplay(
+            number1 = number1,
+            number2 = number2,
+            addButton = {},
+            divideButton = {},
+            subButton = {},
+            multiButton = {},
+        )
     }
 }
