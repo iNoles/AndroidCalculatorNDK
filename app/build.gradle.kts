3a03fc70b68f8acfdf85658b9ba8821dd9212a86
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-20 21:54:01 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.jonathansteele.calculatorndk"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jonathansteele.calculatorndk"
